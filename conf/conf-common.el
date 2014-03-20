09e3da3f828be3daaf91eed74f3c419b3f1daaca
Diep Pham Van <imeo@favadi.com>
2014-03-20 14:18:41 +0700

    remove ibus.el

--- conf/conf-common.el
+++ conf/conf-common.el
@@ -16,17 +16,6 @@
 (smex-initialize)
 (global-set-key (kbd "M-x") 'smex)
 
-;; ibus.el
-(add-to-list 'load-path "~/.emacs.d/ibus")
-(require 'ibus)
-(add-hook 'after-init-hook 'ibus-mode-on)
-;; Use C-SPC for Set Mark command
-(ibus-define-common-key ?\C-\s nil)
-;; Use C-/ for Undo command
-(ibus-define-common-key ?\C-/ nil)
-;; Use C-` to toggle ibus
-(global-set-key (kbd "C-`") 'ibus-toggle)
-
 ;; enable recent files mode
 (require 'recentf)
 (recentf-mode t)
