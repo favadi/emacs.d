6c86072723b30e197bf9a3ebcbded688ede6b0ef
Diep Pham <me@favadi.com>
2015-12-23 22:17:04 +0700

    change font

--- init.el
+++ init.el
@@ -117,7 +117,7 @@
           (exec-path-from-shell-copy-env "GO15VENDOREXPERIMENT")))
       ;; use bash installed from brew
       (setq explicit-shell-file-name "/usr/local/bin/bash")
-      (set-frame-font "Input Mono Compressed 13" t t)
+      (set-frame-font "PragmataPro Mono 12" t t)
       ;; only for railwaycat emacs
       (setq mac-option-modifier 'meta)))
 
