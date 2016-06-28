be11ac97a5559d4af5c7ac4b6d2cdc71c300b268
Diep Pham <imeo@favadi.com>
2016-06-28 18:31:46 +0700

    auto chmod +x script files

--- init.el
+++ init.el
@@ -107,6 +107,10 @@
 ;; confirm to quit
 (setq confirm-kill-emacs #'y-or-n-p)
 
+;; When saving a file that starts with `#!', make it executable.
+(add-hook 'after-save-hook
+            'executable-make-buffer-file-executable-if-script-p)
+
 (autoload 'zap-up-to-char "misc"
   "Kill up to, but not including ARGth occurrence of CHAR.")
 (global-set-key (kbd "M-Z") 'zap-up-to-char)
